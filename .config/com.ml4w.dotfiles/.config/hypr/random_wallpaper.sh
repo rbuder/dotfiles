24cb61185f5d722decfc5be01f399aaacdab797f
Ronald <ronald.buder@gmail.com>
2025-07-11 08:49:44 +0800

    hyprland

---
+++ .config/com.ml4w.dotfiles/.config/hypr/random_wallpaper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# check hyprpaper is running
+pidof hyprpaper > /dev/null || hyprpaper &
+
+WALLPAPER_DIR="$HOME/Pictures/wallpaper"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "CURRENT_WALL")" | shuf -n 1)
+
+hyprctl hyprpaper reload , "$WALLPAPER"
